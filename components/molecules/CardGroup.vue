6b4c62ec38ca3deaa01a463d3574380e2985d1d6
Gourav Kumar <gkumar14@sapient.com>
2020-06-01 11:33:08 +0530

    server side code refectering

--- components/molecules/CardGroup.vue
+++ components/molecules/CardGroup.vue
@@ -1,35 +1,29 @@
 <template>
-<div class="row bg-dark-main" v-if="items.length > 0">
+  <div class="row bg-dark-main" v-if="items.length > 0">
     <app-card v-for="item in items" :item="item" :key="item.id"></app-card>
-</div>
-<div class="row" v-else>
+  </div>
+  <div class="row" v-else>
     <div class="col-12">
-        <strong>No Record Found!</strong>
+      <strong>No Record Found!</strong>
     </div>
-
-    </div>
+  </div>
 </template>
 <script>
- import {mapGetters} from 'vuex';
-import axios from 'axios'
- import Card from '../atoms/Card.vue'
+import { mapGetters } from "vuex";
+import Card from "../atoms/Card.vue";
 export default {
-    async fetch() {
-        const { data } = await axios.get('https://rickandmortyapi.com/api/character');
-         return {items: data.results}
-    },
-    computed: {
-        ...mapGetters({
-            items: 'getResults'
-        }),
-    },
-    components: {
-         appCard: Card
-    }
-}
+  computed: {
+    ...mapGetters({
+      items: "getResults"
+    })
+  },
+  components: {
+    appCard: Card
+  }
+};
 </script>
 <style scoped>
-.bg-dark-main{
-    background-color:#202329 ;
+.bg-dark-main {
+  background-color: #202329;
 }
 </style>
--- store/index.js
+++ store/index.js
@@ -1,6 +1,5 @@
 // export const strict = false;
 
-
 const siteFilters = {
   species: [
     {
@@ -69,180 +68,175 @@ const validate = qry => {
   );
 };
 export const state = () => ({
-        filters: [],
-         results: [],
-         activeFilters: {
-           species: [],
-           gender: [],
-           origin: []
-         },
-         filteredResults: []
+  filters: [],
+  results: [],
+  activeFilters: {
+    species: [],
+    gender: [],
+    origin: []
+  },
+  filteredResults: []
 });
 export const getters = {
-         getResults: state => {
-           if (
-             state.activeFilters.species.length > 0 ||
-             state.activeFilters.gender.length > 0 ||
-             state.activeFilters.origin.length > 0
-           ) {
-             return state.filteredResults;
-           } else {
-             return state.results;
-           }
-         },
-         getActiveTags: state => {
-           let tags = Object.keys(state.activeFilters)
-             .map(item => {
-               return state.activeFilters[item];
-             })
-             .reduce((result, current) => {
-               return [...current, ...result];
-             });
-           return tags;
-         },
-        getFilters: state => {
-           return state.filters;
-         }
+  getResults: state => {
+    if (
+      state.activeFilters.species.length > 0 ||
+      state.activeFilters.gender.length > 0 ||
+      state.activeFilters.origin.length > 0
+    ) {
+      return state.filteredResults;
+    } else {
+      return state.results;
+    }
+  },
+  getActiveTags: state => {
+    let tags = Object.keys(state.activeFilters)
+      .map(item => {
+        return state.activeFilters[item];
+      })
+      .reduce((result, current) => {
+        return [...current, ...result];
+      });
+    return tags;
+  },
+  getFilters: state => {
+    return state.filters;
+  }
 };
 export const mutations = {
-         SET_RESULTS(state, response) {
-           state.results = response;
-         },
-         RENDER_SITE_FILTERS(state, filters) {
-          state.filters = filters;
-         },
-         SET_FILTERED_DATA(state, filteredResult) {
-           state.filteredResults = filteredResult;
-         },
-         SET_SELECTED_FILTERS(state, updatedState) {
-           state.activeFilters = updatedState;
-         },
-        SORT_ITEMS(state, data) {
-          state.filteredResults = data.filteredResults;
-          state.results = data.results;
-        }
-
+  SET_RESULTS(state, response) {
+    state.results = response;
+  },
+  RENDER_SITE_FILTERS(state, filters) {
+    state.filters = filters;
+  },
+  SET_FILTERED_DATA(state, filteredResult) {
+    state.filteredResults = filteredResult;
+  },
+  SET_SELECTED_FILTERS(state, updatedState) {
+    state.activeFilters = updatedState;
+  },
+  SORT_ITEMS(state, data) {
+    state.filteredResults = data.filteredResults;
+    state.results = data.results;
+  }
 };
 export const actions = {
-         async nuxtServerInit({ commit,dispatch }) {
-           commit("RENDER_SITE_FILTERS", siteFilters);
-         },
-         loadData: async ({ commit }) => {
-           axios
-             .get("https://rickandmortyapi.com/api/character")
-             .then(response => {
-               return response.data;
-             })
-             .then(data => {
-               commit("SET_RESULTS", data.results);
-             });
-         },
-         search: ({ commit, $axios }, qry) => {
-           axios
-             .get("https://rickandmortyapi.com/api/character" + validate(qry))
-             .then(response => {
-               return response.data;
-             })
-             .then(data => {
-               commit("SET_RESULTS", data.results);
-             })
-             .catch(function() {
-               // handle error
-               commit("SET_RESULTS", []);
-             });
-         },
-         applyFilter: ({ commit, state }, filter) => {
-           if (filter == null && typeof filter != Object) {
-             throw new Error(`As argument use an object`);
-           }
+  async nuxtServerInit({ commit, dispatch }) {
+    const { data } = await axios.get(
+      "https://rickandmortyapi.com/api/character"
+    );
 
-           // active filters
-           let currentFilter;
-           if (filter.status) {
-             currentFilter = {
-               ...state.activeFilters,
-               [filter.category]: [
-                 ...state.activeFilters[filter.category],
-                 filter.value
-               ]
-             };
-           } else {
-             currentFilter = {
-               ...state.activeFilters,
-               [filter.category]: [...state.activeFilters[filter.category]]
-             };
-             const index = currentFilter[filter.category].indexOf(filter.value);
-             if (index > -1) {
-               currentFilter[filter.category].splice(index, 1);
-             }
-           }
-           commit("SET_SELECTED_FILTERS", currentFilter);
+    return Promise.all([
+      commit("RENDER_SITE_FILTERS", siteFilters),
+      commit("SET_RESULTS", data.results)
+    ]);
+  },
+  search: ({ commit, $axios }, qry) => {
+    axios
+      .get("https://rickandmortyapi.com/api/character" + validate(qry))
+      .then(response => {
+        return response.data;
+      })
+      .then(data => {
+        commit("SET_RESULTS", data.results);
+      })
+      .catch(function() {
+        // handle error
+        commit("SET_RESULTS", []);
+      });
+  },
+  applyFilter: ({ commit, state }, filter) => {
+    if (filter == null && typeof filter != Object) {
+      throw new Error(`As argument use an object`);
+    }
 
-           //filter data based on user selection
-           let filteredResults;
+    // active filters
+    let currentFilter;
+    if (filter.status) {
+      currentFilter = {
+        ...state.activeFilters,
+        [filter.category]: [
+          ...state.activeFilters[filter.category],
+          filter.value
+        ]
+      };
+    } else {
+      currentFilter = {
+        ...state.activeFilters,
+        [filter.category]: [...state.activeFilters[filter.category]]
+      };
+      const index = currentFilter[filter.category].indexOf(filter.value);
+      if (index > -1) {
+        currentFilter[filter.category].splice(index, 1);
+      }
+    }
+    commit("SET_SELECTED_FILTERS", currentFilter);
 
-           const filterKeys = Object.keys(state.activeFilters);
-           filteredResults = state.results.filter(item => {
-             return filterKeys.every(key => {
-               if (!state.activeFilters[key].length) {
-                 return true;
-               }
-               if (key == "name") {
-                 return item[key].includes(state.activeFilters[key]);
-               } else {
-                 return state.activeFilters[key].includes(item[key]);
-               }
-             });
-           });
-           commit("SET_FILTERED_DATA", filteredResults);
-         },
-         removeFilter: ({ commit, state }, filter) => {
-           let event = new Event("change");
-           document.querySelector(
-             ".filters input[id='" + filter.value + "']"
-           ).checked = false;
-           document
-             .querySelector(".filters input[id='" + filter.value + "']")
-             .dispatchEvent(event);
+    //filter data based on user selection
+    let filteredResults;
 
-           let currentFilter = { ...state.activeFilters };
-           const index = currentFilter[filter.category].indexOf(filter.value);
-           if (index > -1) {
-             currentFilter[filter.category].splice(index, 1);
-           }
+    const filterKeys = Object.keys(state.activeFilters);
+    filteredResults = state.results.filter(item => {
+      return filterKeys.every(key => {
+        if (!state.activeFilters[key].length) {
+          return true;
+        }
+        if (key == "name") {
+          return item[key].includes(state.activeFilters[key]);
+        } else {
+          return state.activeFilters[key].includes(item[key]);
+        }
+      });
+    });
+    commit("SET_FILTERED_DATA", filteredResults);
+  },
+  removeFilter: ({ commit, state }, filter) => {
+    let event = new Event("change");
+    document.querySelector(
+      ".filters input[id='" + filter.value + "']"
+    ).checked = false;
+    document
+      .querySelector(".filters input[id='" + filter.value + "']")
+      .dispatchEvent(event);
 
-           commit("SET_SELECTED_FILTERS", currentFilter);
-         },
-         sortBy: function({ commit, state }, payload) {
-           // Check, what direction now should be
-           const sortDirection =
-             payload.direction === "descending" ? "desc" : "asc";
-           const isFilterEnabled = state.filteredResults.length > 0;
-           let data = {
-             filteredResults: [...state.filteredResults],
-             results: [...state.results]
-           };
-           if (sortDirection === "asc") {
-             if (isFilterEnabled) {
-               data.filteredResults.sort(function(a, b) {
-                 return a.id - b.id;
-               });
-             } else {
-               data.results.sort(function(a, b) {
-                 return a.id - b.id;
-               });
-             }
-           } else {
-             if (isFilterEnabled) {
-               data.filteredResults.sort(function(a, b) {
-                 return b.id - a.id;
-               });
-             } else {
-               data.results.sort(function(a, b) {
-                 return b.id - a.id;
-               });
-             }
-           }
-           commit("SORT_ITEMS", data);
-         }
-       };
+    let currentFilter = { ...state.activeFilters };
+    const index = currentFilter[filter.category].indexOf(filter.value);
+    if (index > -1) {
+      currentFilter[filter.category].splice(index, 1);
+    }
+
+    commit("SET_SELECTED_FILTERS", currentFilter);
+  },
+  sortBy: function({ commit, state }, payload) {
+    // Check, what direction now should be
+    const sortDirection = payload.direction === "descending" ? "desc" : "asc";
+    const isFilterEnabled = state.filteredResults.length > 0;
+    let data = {
+      filteredResults: [...state.filteredResults],
+      results: [...state.results]
+    };
+    if (sortDirection === "asc") {
+      if (isFilterEnabled) {
+        data.filteredResults.sort(function(a, b) {
+          return a.id - b.id;
+        });
+      } else {
+        data.results.sort(function(a, b) {
+          return a.id - b.id;
+        });
+      }
+    } else {
+      if (isFilterEnabled) {
+        data.filteredResults.sort(function(a, b) {
+          return b.id - a.id;
+        });
+      } else {
+        data.results.sort(function(a, b) {
+          return b.id - a.id;
+        });
+      }
+    }
+    commit("SORT_ITEMS", data);
+  }
+};
